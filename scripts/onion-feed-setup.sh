1c18b67a2f3fa81d4bdcfdf46926916390c9f8f6
Boussaada Mohamed Ayoub <mohmaed-ayoub.boussaada@sagemcom.com>
2022-11-19 17:06:39 +0100

    Update gphoto and libgphoto to latest versions

--- scripts/onion-feed-setup.sh
+++ scripts/onion-feed-setup.sh
@@ -9,3 +9,8 @@ echo "Preparing package feeds..."
 ./scripts/feeds uninstall bluez-examples bluez-libs bluez-utils pulseaudio-daemon pulseaudio-profiles pulseaudio-tools avrdude
 ./scripts/feeds install -p onion bluez-examples bluez-libs bluez-utils pulseaudio-daemon pulseaudio-profiles pulseaudio-tools avrdude
 
+# Apply patch to update libgphoto from 2.5.17 to 2.5.30 gphoto to 2.5.17 to 2.5.28
+if ! patch -R -p 1 -s -f --directory=feeds/packages --dry-run < "files/patches/gphoto-v28-libgphoto-v30.patch"; then
+  patch -p 1 --directory=feeds/packages < "files/patches/gphoto-v28-libgphoto-v30.patch"
+fi
+
